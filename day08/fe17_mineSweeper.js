74bf101e8ac6e16e4d684def8104a43d42f03cad
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-09-20 13:21:41 +0900

    폭탄

--- day08/fe17_mineSweeper.js
+++ day08/fe17_mineSweeper.js
@@ -8,48 +8,61 @@ document.addEventListener('DOMContentLoaded', () => {
     let cnt = 0;
 
     // 초기화
-    // const init = () => {
-    //     flag = false;
-    //     cnt = 0;
-    //     btn.innerHTML = '폭탄 섞기';
-    // }
+    const init = () => {
+        cnt = 0;
+        flag = false;
+        btn.disabled = false;
+    }
 
     // 폭탄섞기 버튼
     btn.addEventListener('click', (e) => {
-        // if(btn.innerHTML == '다시하기') init();
+        if(btn.innerHTML == '다시하기' || btn.innerHTML == '성공!') {
+            for(let col of cols) {
+                col.innerHTML = '';
+            }
+            // cnt = 0;
+            // flag = false;
+            // btn.disabled = false;
+            init();
+        }
+
         // shuffle
         arrNum.sort(() => Math.random() - 0.5);
         flag = true;
         btn.innerHTML = '게임중...';
         console.log(arrNum);
 
-        if(flag) btn.disabled = true;
+        if(flag) btn.disabled = true; // 게임중 버튼 조작(폭탄 섞기) 금지
+
+        // 보드선택
+        for(let col of cols) {
+            col.addEventListener('click', () => {
+                if (!flag) return;  // 시작 전, 끝난 후 더이상 눌려지지 않도록
+
+                idx = parseInt(col.getAttribute('id').replace('col', '')); // id가 col3이라면 'col'을 제거하고 3만 정수로 변환해 idx에 넣음
+                if (col.innerHTML !== '') return;
+                
+                if (arrNum[idx] == 0) { // 폭탄 없는 곳
+                    cnt++; // 몇 번 눌렀는지 확인
+                    col.innerHTML = '💖';
+                    if(cnt == 8) { // 지뢰빼고 다 찾음
+                        btn.innerHTML = '성공!';
+                        // btn.disabled = false;
+                        // flag = false;
+                        init();
+                    }
+                }
+                else {
+                    col.innerHTML = '💥';
+                    btn.innerHTML = '다시하기';
+                    // cnt = 0;
+                    // flag = false;
+                    // btn.disabled = false;
+                    init();
+                }
+                console.log(cnt);
+            });
+        }
     });
-
-    // 보드선택
-    for(let col of cols) {
-        col.addEventListener('click', () => {
-            idx = parseInt(col.getAttribute('id').replace('col', '')); // id가 col3이라면 'col'을 제거하고 3만 정수로 변환해 idx에 넣음
-            if (arrNum[idx] == 0) {
-                cnt++;
-                col.innerHTML = '💖';
-            }
-        });
-    }
-
-
-    //         if(!flag) cnt == 0;
-    //         cnt++;
-    //             col.backgroundColor = 'gray';
-    //             if (cnt == 8) {
-    //                 btn.innerHTML = '다시하기';
-    //                 flag = false;
-    //             }
-    //         else {
-    //             col.backgroundColor = 'red';
-    //             col.innerHTML = '💥'
-    //             btn.innerHTML = '다시하기';
-    //             flag = false
-    //         }
-    
 });
+
