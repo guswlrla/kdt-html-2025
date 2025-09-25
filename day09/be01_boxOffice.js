043e51fde509704eb0c67f8de2882154bbcd6c05
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-09-25 18:05:18 +0900

    api키 조심

---
+++ day09/be01_boxOffice.js
@@ -0,0 +1,58 @@
+// 어제 날짜 가져오기
+const getYesterday = () => {
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1); // 어제 날짜
+    console.log(yesterday);
+
+    let year = yesterday.getFullYear(); // 연도 4자리
+    let mon = yesterday.getMonth() + 1; // 월 1~12
+    let day = yesterday.getDate(); // 일 1~31
+
+    return (year + '-' + String(mon).padStart(2,'0') + '-' + String(day).padStart(2,'0'));
+}
+
+const getrankInten = (rankInten) => {
+    let sprankInten;
+    if(rankInten > 0) sprankInten = `<span class="spr">▲${rankInten}</span>`
+    else if (rankInten < 0) sprankInten = `<span class="spb">▼${Math.abs(rankInten)}</span>`
+    else sprankInten = '<sapn class="sp">-</span>'
+    return sprankInten;
+}
+
+// 박스 오피스 가져오기
+const getData = (gdt, box) => {
+    let apiKey = '1d3aeb0dbc329db4ed3a1ad87e4337a6';
+    let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apiKey}&targetDt=${gdt}`;
+
+    // fetch
+    fetch(url) // 주소를 검색해서 불러옴
+        .then(resp => resp.json())
+        .then(data => {
+            let boxs = data.boxOfficeResult.dailyBoxOfficeList; // 배열
+            let tags = boxs.map(item => `<li class="boxli">
+                                        <span class="boxrank">${item.rank}</span>
+                                        ${getrankInten(item.rankInten)}
+                                        <span>${item.movieNm}</span>
+                                        </li>`).join(''); // 10개의 배열, join() - 배열을 묶어줌
+            box.innerHTML = tags;
+        }) // .then이 끝나야 .then이 실행 됨
+        .catch(err => console.log(err));
+
+    console.log(url);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 요소 가져오기
+    const dt = document.querySelector("#dt");
+    const box = document.querySelector("#box");
+
+    // 날짜가 변경될 때
+    dt.addEventListener('change', () => {getData(dt.value.replaceAll('-', ''), box)});
+    
+    // 어제 날짜
+    dt.value = getYesterday();
+    dt.setAttribute('max', getYesterday());
+    
+    // 초기 박스오피스 가져오기
+    getData(dt.value.replaceAll('-', ''), box);
+});
---
+++ day10/be02_tour.js
@@ -0,0 +1,39 @@
+// 데이터 가져오기
+const getData = (txt1) => {
+    let apiKey = "";
+    let keyWord = encodeURIComponent(txt1);
+    let url = `https://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?serviceKey=${apiKey}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&keyword=${keyWord}&_type=json`;
+
+    fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            let imgs = data.response.body.items.item;
+            let tags = imgs.map(item => `<div class="imgs">
+                                            <img src='${item.galWebImageUrl}'>
+                                            ${item.galTitle}
+                                            ${item.galPhotographyLocation}
+                                            </div>`).join('');
+            content.innerHTML = tags;
+        })
+        .catch(err => console.log(err));
+    
+    console.log(url);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 요소 선택
+    const txt1 = document.querySelector('#txt1');
+    const btOk = document.querySelector('#btOk');
+    const btCancel = document.querySelector('#btCancel');
+    const content = document.querySelector('#content');
+
+    // 확인 버튼이 눌려지면 데이터 가져오기
+    btOk.addEventListener('click', () => {
+        getData(txt1.value);
+    });
+
+    // 버튼이 눌려지면 텍스트 내용과 content내용 지우기
+    btCancel.addEventListener('click', () => {
+        content.innerHTML = '';
+    })
+});
